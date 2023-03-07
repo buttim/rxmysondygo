dab09264636012b58595e77723757a9ed8537d07
buttim <buttim@bitbucket.org>
2023-03-08 00:41:49 +0100

    adds remote configuration of the TTGOs

--- rxmysondygo.py
+++ rxmysondygo.py
@@ -2,7 +2,6 @@ import serial
 import json
 import os
 import sys
-import time
 import threading
 import http.server
 from datetime import datetime
@@ -14,10 +13,12 @@ from urllib.parse import parse_qs
 
 hostName = "0.0.0.0"
 serverPort = 5556
+tipi =['RS41','M20','M10','PIL','DFM']
 data = {}
 fine = False
 files = []
-
+ttgo = {}
+ser = {}
 
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
@@ -29,9 +30,24 @@ def json_serial(obj):
 
 class MyServer(SimpleHTTPRequestHandler):
     protocol_version = "HTTP/1.0"
-    
+
     def log_message(self, format, *args):
         pass
+        
+    def do_POST(self):
+        if self.path == '/cfg':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            data = post_data.decode('utf-8')
+            #~ print(data)
+            cfg = json.loads(data)
+            self.send_response(200)
+            self.end_headers()
+            for i in cfg:
+                tipo=tipi.index(cfg[i]['type'])+1
+                cmd=f'o{{f={cfg[i]["freq"]}/tipo={tipo}}}o'
+                #~ print(cmd,flush=True)
+                ser[i].write(cmd.encode())
 
     def do_GET(self):
         try:
@@ -39,12 +55,10 @@ class MyServer(SimpleHTTPRequestHandler):
             params = parse_qs(uri.query)
             if uri.path == '/data':
                 self.send_response(200)
-                self.send_header("Content-type", "text/plain")
                 self.send_header("Content-type", "application/json")
                 self.send_header("Access-Control-Allow-Origin", "*")
                 self.end_headers()
                 start = params['from'][0] if 'from' in params else ''
-                # print(data,flush=True)
                 res = {
                     id: {
                         'type': sondeData['type'],
@@ -56,6 +70,11 @@ class MyServer(SimpleHTTPRequestHandler):
                 }
                 self.wfile.write(bytes(json.dumps(res, default=json_serial),
                                  "utf-8"))
+            elif self.path == '/cfg':
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                self.wfile.write(bytes(json.dumps(ttgo), "utf-8"))
             elif self.path == '/':
                 self.path = "web/rxmysondygo.html"
                 http.server.SimpleHTTPRequestHandler.do_GET(self)
@@ -94,66 +113,34 @@ files = os.listdir(os.getcwd() + "/web")
 t = threading.Thread(target=webServerThread)
 t.start()
 
-isFile = False
-separator = '/'
 try:
-    if False and len(sys.argv) == 2:
-        try:
-            print(os.stat(sys.argv[1]))
-            isFile = True
-            separator = ','
-        except FileNotFoundError:
-            isFile = False
-            separator = '/'
-
     n = 0
-    ser = []
     for i in range(len(sys.argv)-1):
-        ser.append(open(sys.argv[i+1], "r") if isFile
-                   else serial.Serial(sys.argv[i+1], 9600))
+        ser[sys.argv[i+1]]=(serial.Serial(sys.argv[i+1], 9600))
         n += 1
-    if isFile:
-        ser[0].readline()  # butta via intestazione
     while True:
-        for i in range(n):
-            s = ser[i].readline()
-            if not isFile:
-                s = s.decode()
-            s = s.strip()
-            if s == '':
-                break
-            a = s.split(separator)
-            if isFile:
-                id = a[1]
-                frame = {
-                    'datetime': a[0],
-                    'lat': float(a[3]),
-                    'lon': float(a[4]),
-                    'alt': float(a[5]),
-                }
-                print(frame)
-                time.sleep(0.1)
-            else:
-                if (a[0] != '1') or float(a[4]) == 0:
-                    continue
-                id = a[3]
-                if id == 'No data':
-                    continue
-                frame = {
-                    'datetime': datetime.now().isoformat() + 'Z',
-                    'lat': float(a[4]),
-                    'lon': float(a[5]),
-                    'alt': float(a[6]),
-                    'rssi':float(a[8])
-                }
+        for i in ser:
+            s = ser[i].readline().decode().strip()
+            a = s.split('/')
+
+            ttgo[ser[i].name] = {'type': a[1], 'freq': float(a[2])}
+            if (a[0] != '1') or float(a[4]) == 0:
+                continue
+            id = a[3]
+            if id == 'No data':
+                continue
+            frame = {
+                'datetime': datetime.now().isoformat() + 'Z',
+                'lat': float(a[4]),
+                'lon': float(a[5]),
+                'alt': float(a[6]),
+                'rssi': float(a[8])
+            }
             if (id not in data):
-                if isFile:
-                    data[id] = {'type': a[12], 'freq': a[13], 'frames': []}
-                else:
-                    data[id] = {'type': a[1], 'freq': a[2], 'frames': []}
+                data[id] = {'type': a[1], 'freq': a[2], 'frames': []}
             data[id]['frames'].append(frame)
 except serial.SerialException:
-    print(f'La seriale {sys.argv[n+1]} non esiste')
+    print(f'La seriale {sys.argv[n+1]} non esiste?')
     fine = True
 except Exception as x:
     exc_type, exc_obj, exc_tb = sys.exc_info()
